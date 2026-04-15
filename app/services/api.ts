baaa49698d02ef6ea8da80df77bb076aa5829e4d
Devyansh Grover <devyansh.grover348@gmail.com>
2026-04-15 17:08:34 +0530

    api.js updated

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,4 +1,5 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000";
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://tfp-backend.onrender.com";
 const API_URL = `${BASE_URL}/api`;
 
 type FetchApiOptions = RequestInit & {
